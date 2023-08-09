add17325543c6cf9a807a5674b12ff3bfd123d1c
westtly25 <westtly25@gmail.com>
2023-08-09 08:03:18 +0300

    Update and fixe

--- Assets/Scripts/Runtime/Audio System/AudioContainer.cs
+++ Assets/Scripts/Runtime/Audio System/AudioContainer.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
+using Unity.Collections;
 
 namespace Assets.Code.Scripts.Runtime.Audio_System
 {
     [CreateAssetMenu(menuName = "Match3 / Audio Container", fileName = "Audio Container", order = 1)]
     public class AudioContainer : ScriptableObject
     {
-        [SerializeField]
+        [SerializeField, ReadOnly]
         private AudioItem[] AudioItems; 
     }
 }
--- Assets/Scripts/Runtime/Audio System/SoundType.cs
+++ Assets/Scripts/Runtime/Audio System/SoundType.cs
@@ -1,13 +1,13 @@
 ﻿namespace Assets.Code.Scripts.Runtime.Audio_System
 {
-    public enum SoundType
+    public enum SoundType : byte
     {
         Main_Theme_Music,
         UI_Button_Click_Sound,
         UI_Slider_Move_Sound,
         UI_Togle_Click_Sound,
-
+        UI_Shop_Bytton_Click_Sound,
         Gameplay_Click_Sound,
-        Blacksmith_Work_Sound,
+        Gameplay_Task_Done_Sound,
     }
 }
