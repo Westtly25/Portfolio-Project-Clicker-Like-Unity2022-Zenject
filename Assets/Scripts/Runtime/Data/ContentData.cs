e108bea7aa0904fa0cc684d6e8015ca202e3e355
westtly25 <westtly25@gmail.com>
2023-08-09 08:44:20 +0300

    Update and remove

--- Assets/Scripts/Runtime/Data/ContentData.cs
+++ Assets/Scripts/Runtime/Data/ContentData.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assets.Scripts.Runtime.Data
 {
     [Serializable]
     public class ContentData
     {
-        public TopicType topicType;
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public float TimeToCreate { get; set; }
+        public TopicType TopicType { get; set; }
     }
 }
